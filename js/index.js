c5e59faeacd808ca1144264069af1c92ef0566ca
Ankur Charan <ankurcharan98@gmail.com>
2020-06-30 01:39:41 +0530

    fix one
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- js/index.js
+++ js/index.js
@@ -1,26 +1,61 @@
-function heyy()
-{
-    if( $('#compulsoryPA').is(":checked") )
-        console.log('vv');
+const bundled = document.getElementById('bundled');
+const payears = document.getElementById('payears');
+const compulsoryPAallow = document.getElementById('compulsoryPA');
+
+// variables
+let engineCC = 0;
+let compPA = 275;
+
+function showActOnly() {
+    // d-none to hide in bootstrap
+    $('#compulsoryPAform').addClass('d-none');
+    if($('#compulsoryPA').is(":checked"))
+        compPA = 275;
     else
-        console.log('aa');
-
-    if($('#bundled').prop('selectedIndex') == 0)
-        $('#xx').html('275');
-    else if($('#bundled').prop('selectedIndex') == 1)
-    {
-        let option = `
-        <div class="form-group">
-            <label for="years">Act Only/Bundled</label>
-            <select class="form-control" id="years">
-            <option>1</option>
-            <option>2</option>
-            <option selected>3<option>
-            </select>
-        </div>
-        `;
-
-        $('#xx').html(option);
-        $('#xx').append('<label>275 * option</label>');
-    }
+        compPA = 0;
+    $('#compulsoryPAamount').html(`&#x20b9; ${compPA}`);
 }
+function showBundled() {
+    $('#compulsoryPAform').removeClass('d-none');
+
+    const years = payears.value;
+    compPA = 275 * years;
+    $('#compulsoryPAamount').html(`&#x20b9; ${compPA}`);
+}
+
+bundled.addEventListener('change', (event) => {
+    
+    console.log(typeof(event.target.value), event.target.value);
+    if(event.target.value === 'act_only') {
+        showActOnly();
+    } else if(event.target.value === 'bundled') {
+        if($('#compulsoryPA').is(":checked"))
+            showBundled();
+    }
+})
+
+payears.addEventListener('change', (e) => {
+    console.log('PA Years: ', e.target.value);
+    const years = parseInt(e.target.value);
+    compPA = 275 * years;
+    $('#compulsoryPAamount').html(`&#x20b9; ${compPA}`);
+});
+
+compulsoryPAallow.addEventListener('change', (e) => {
+    console.log(e.target.value);
+    console.log(typeof(e.target.value));
+
+    if($('#compulsoryPA').is(":checked")) {
+        
+        if(bundled.value === 'act_only') {
+            showActOnly();
+        } else if(bundled.value === 'bundled') {
+            showBundled();
+        }
+    } else {
+        $('#compulsoryPAform').addClass('d-none');
+        compPA = 0;
+        $('#compulsoryPAamount').html(`&#x20b9; ${compPA}`);
+    }
+        
+})
