28d03d26d31d77b8bd07f83f2e2f9b5dd15be385
Ankur Charan <ankurcharan98@gmail.com>
2020-07-01 09:56:11 +0530

    package
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- js/index.js
+++ js/index.js
@@ -5,6 +5,13 @@ const legalPerson = document.getElementById('legalPerson');
 const paPerson = document.getElementById('paPerson');
 const PACoverAmount = document.getElementById('PACoverAmount');
 const engineCCInput = document.getElementById('engineCC');
+// Package 
+const packageAge = document.getElementById('age');
+const zone = document.getElementById('zone');
+const IDVvalue = document.getElementById('IDVvalue');
+const discountPackage = document.getElementById('discountPackage');
+const ncb = document.getElementById('ncb');
+// Package Ends
 
 // rates
 const privateCarBasicRate = [
@@ -148,4 +155,107 @@ PACoverAmount.addEventListener('change', (e) => {
     showPACoverAmount();
 
     showSumThirdParty();
+})
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////
+//////////////////////////////////////
+//////////////////////////////////////
+////////////// PACKAGE ///////////////
+//////////////////////////////////////
+//////////////////////////////////////
+//////////////////////////////////////
+
+let basicPremium = 0;
+let discount = 0;
+let discPrem = 0;
+
+function getPackageRate() {
+    // (zone, cc, age)
+    const ownDamagePremiumRate = [
+        [
+            [3.127, 3.283, 3.362],
+            [3.283, 3.447, 3.529],
+            [3.440, 3.612, 3.698]
+        ],
+        [
+            [3.039, 3.191, 3.267],
+            [3.191, 3.351, 3.430],
+            [3.343, 3.510, 3.594]
+        ]
+    ];
+
+    let x = 0;
+    if(engineCC <= 1000) {
+        x = 0;
+    } else if(engineCC <= 1500) {
+        x = 1;
+    } else {
+        x = 2;
+    }
+
+    let y = age.value;
+
+    return ownDamagePremiumRate[zone.value][x][y];
+}
+
+function showPackageRate() {
+    const x = getPackageRate();
+    $('#packageRate').html(`Rate: ${x}%`);
+
+    showBasicPremium();
+}
+
+packageAge.addEventListener('change', (e) => {
+    console.log('Age: ', e.target.value);
+    showPackageRate();
+});
+
+zone.addEventListener('change', () => {
+    showPackageRate();
+});
+
+engineCCInput.addEventListener('change', () => {
+    showPackageRate();
+});
+
+function showBasicPremium() {
+    const rate = getPackageRate() / 100;
+    const idv = parseInt(IDVvalue.value);
+
+    basicPremium = rate * idv;
+    $('#basicPrem').html(`Basic Preimum: &#x20b9; ${basicPremium}`);  
+    
+    showDiscountPremium();
+}
+
+IDVvalue.addEventListener('change', () => {
+    showPackageRate();
+    showBasicPremium();
+})
+function showDiscountPremium() {
+    // discount = parseFloat(discountPackage.value);
+    discPrem = (100 - discount) / 100 * basicPremium;
+    $('#discountPremium').html(`Discount Premium: &#x20b9; ${discPrem}`)
+    showNCBPremium();
+}
+discountPackage.addEventListener('change', (e) => {
+    discount = parseFloat(e.target.value);
+    showDiscountPremium();
+});
+function showNCBPremium() {
+    const ncbVal = parseFloat(ncb.value);
+    const zz = discPrem * (100 - ncbVal) / 100;
+    $('#ncbPremium').html(`NCB Premium: &#x20b9; ${zz}`);
+}
+ncb.addEventListener('change', (e) => {
+    showNCBPremium();
 })
